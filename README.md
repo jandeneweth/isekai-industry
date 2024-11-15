011997c6172ae5821f90e9adfa203fcf0b30f316
jandeneweth <62950206+jandeneweth@users.noreply.github.com>
2024-11-15 16:00:43 +0100

    Basic setup

---
+++ README.md
@@ -0,0 +1,7 @@
+# Isekai Industry
+
+Reborn into the past, time travellers influence the Medieval age.
+
+## Acknowledgements
+
+The [EU4 to Vic3 converter](https://forum.paradoxplaza.com/forum/threads/eu4-to-vic3-converter-thread.1475054/) tool, for providing a solid basis for the mod.
---
+++ scripts/generate_deployment.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Script to (re)generate the deployable mod.
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+echo "Script location: ${SCRIPT_DIR}"
+STATIC_DIR="${SCRIPT_DIR}/../sources/static"
+echo "Static sources directory: ${STATIC_DIR}"
+BUILD_DIR="${SCRIPT_DIR}/../build"
+echo "Build directory: ${BUILD_DIR}"
+BUILD_MOD_DIR="${BUILD_DIR}/isekai-industry"
+echo "Build mod directory: ${BUILD_MOD_DIR}"
+
+# Remove the existing files
+rm -rf ${BUILD_DIR}/*
+
+# Copy the static files, in order of overwrites
+cp -r ${STATIC_DIR}/converted ${BUILD_MOD_DIR}
+cp -r ${STATIC_DIR}/metadata ${BUILD_MOD_DIR}
